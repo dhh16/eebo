e408f3487a32363347c9eb8326f435e48cdefa37
Jukka Suomela <jukka.suomela@iki.fi>
2016-05-16 18:24:08 +0300

    Smaller limit.

--- eebo-tcp-similarity/analyse-shakespeare.sh
+++ eebo-tcp-similarity/analyse-shakespeare.sh
@@ -2,5 +2,5 @@
 
 mkdir html-shakespeare
 cp html/style.css html-shakespeare/
-./analyse output-shakespeare/results.xml output/raw-256.txt Shakespeare < eebo-tcp.txt
-./analyse.py html-shakespeare output-shakespeare/results.xml
+./analyse output/results-shakespeare.xml output/raw-128.txt Shakespeare < eebo-tcp.txt
+./analyse.py html-shakespeare output/results-shakespeare.xml
