eb80f1f6abb741d5e5b0e5bddae27dc2a47e9a6a
Jukka Suomela <jukka.suomela@iki.fi>
2016-05-16 21:26:25 +0300

    Minor fix.

--- eebo-tcp-similarity/analyse-1599.sh
+++ eebo-tcp-similarity/analyse-1599.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-mkdir html-1599
+mkdir -p html-1599
 cp html/style.css html-1599/
 ./analyse output-1599/results.xml output-1599/raw-256.txt < eebo-tcp-1599.txt
 ./analyse.py html-1599 output-1599/results.xml
--- eebo-tcp-similarity/analyse-shakespeare.sh
+++ eebo-tcp-similarity/analyse-shakespeare.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-mkdir html-shakespeare
+mkdir -p html-shakespeare
 cp html/style.css html-shakespeare/
 ./analyse output/results-shakespeare.xml output/raw-128.txt Shakespeare < eebo-tcp.txt
 ./analyse.py html-shakespeare output/results-shakespeare.xml
