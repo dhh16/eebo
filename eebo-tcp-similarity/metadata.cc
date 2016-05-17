c9684a317e53c00ffa65762bd85c110be23a526f
Jukka Suomela <jukka.suomela@iki.fi>
2016-05-17 13:35:20 +0300

    Metadata extraction.

--- eebo-tcp-similarity/metadata.cc
+++ eebo-tcp-similarity/metadata.cc
@@ -1,17 +1,35 @@
 #include <cassert>
+#include <cctype>
 #include <cstring>
-#include <algorithm>
-#include <fstream>
 #include <iostream>
 #include <string>
 #include <vector>
-#include <unordered_set>
 #include "pugixml.hpp"
-#include "utf8.h"
 #include "char-map.h"
-#include "timer.h"
 
 const char* xml_dir = "tcp-xml";
+
+enum {
+    PUBLISHER, PLACE, DATE, N_PARTS
+};
+
+
+std::string clean(std::string x) {
+    std::string y;
+    bool seen_ws = false;
+    for (char c : x) {
+        if (std::isspace(c)) {
+            seen_ws = true;
+        } else {
+            if (seen_ws && y.size()) {
+                y += ' ';
+            }
+            seen_ws = false;
+            y += c;
+        }
+    }
+    return y;
+}
 
 
 struct Text : pugi::xml_tree_walker
@@ -29,6 +47,9 @@ struct Text : pugi::xml_tree_walker
     std::string date;
     std::string extent;
     std::string publ;
+    std::string publ_parts[N_PARTS];
+    int depth_prev;
+    int current;
 };
 
 std::string Text::get_full_name() const {
@@ -37,7 +58,7 @@ std::string Text::get_full_name() const {
     fullname += id;
     fullname += ".xml";
     return fullname;
-}
+}  
 
 void Text::fail(std::string msg) {
     std::cerr << get_full_name() << ": " << msg << std::endl;
@@ -62,23 +83,50 @@ void Text::parse() {
     date = fileDesc.child("editionStmt").child("edition").child_value("date");
     extent = biblFull.child_value("extent");
     pugi::xml_node publStmt = biblFull.child("publicationStmt");
+    depth_prev = 0;
+    current = -1;
     publStmt.traverse(*this);
 }
 
 void Text::dump() {
-    std::cout << title << "\n";
-    std::cout << author << "\n";
-    std::cout << date << "\n";
-    std::cout << publ << "\n";
-    std::cout << extent << "\n";
+    std::vector<std::string> p;
+    p.push_back(id);
+    p.push_back(title);
+    p.push_back(author);
+    p.push_back(date);
+    p.push_back(extent);
+    p.push_back(publ);
+    for (int i = 0; i < N_PARTS; ++i) {
+        p.push_back(publ_parts[i]);
+    }
+    for (int i = 0; i < p.size(); ++i) {
+        if (i) {
+            std::cout << "\t";
+        }
+        std::cout << clean(p[i]);
+    }
     std::cout << "\n";
 }
 
 bool Text::for_each(pugi::xml_node& node) {
     using namespace pugi;
-    if (node.type() == node_pcdata) {
+    if (node.type() == node_element && depth() <= depth_prev) {
+        if (!strcmp(node.name(), "publisher")) {
+            current = PUBLISHER;
+        } else if (!strcmp(node.name(), "pubPlace")) {
+            current = PLACE;
+        } else if (!strcmp(node.name(), "date")) {
+            current = DATE;
+        } else {
+            current = -1;
+        }
+        depth_prev = depth();
+    } else if (node.type() == node_pcdata) {
         const char* p = node.value();
         publ += p;
+        if (current != -1) {
+            publ_parts[current] += p;
+        }
     }
     return true;
 }
@@ -86,9 +134,7 @@ bool Text::for_each(pugi::xml_node& node) {
 
 int main() {
     std::ios_base::sync_with_stdio(0);
-    Timer timer;
     std::vector<Text> texts;
-    timer.log("Read file list");
     std::string id;
     while (std::cin >> id) {
         int t = texts.size();
@@ -96,7 +142,6 @@ int main() {
     }
     const int n = texts.size();
 
-    timer.log("Parse files");
     #pragma omp parallel for
     for (int t = 0; t < n; ++t) {
         texts[t].parse();
@@ -105,6 +150,4 @@ int main() {
     for (int t = 0; t < n; ++t) {
         texts[t].dump();
     }
-
-    timer.log("Done");
 }
